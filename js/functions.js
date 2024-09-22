6feede9102d77503b2d0257fab52af4147139156
Yarnara <edeven.gothery@gmail.com>
2024-09-22 07:21:07 +0700

    feat: добавляет функцию с проверкой на палиндромы

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,21 @@
 const checkLength = (line = '', maxLendth = 1) => (line.length <= maxLendth);
 
 checkLength();
+
+
+//Проверяет на палиндромы
+
+const checkPalindrome = (string = '') => {
+  string = string.replaceAll(' ', '');
+  string = string.toLowerCase();
+
+  let reverseLine = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseLine += string[i];
+  }
+
+  return string === reverseLine;
+};
+
+checkPalindrome('');
