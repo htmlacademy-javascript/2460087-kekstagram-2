de168af2d3f709f5a4ad8f6ce52858b8cb8ed351
Yarnara <edeven.gothery@gmail.com>
2024-12-12 20:47:09 +0700

    добавляет просмотр большой фотографии

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 import { createPhotos } from './content-generator.js';
 import { renderThumbnails } from './render-thumbnails.js';
+import { initGallery, closedBigPicture } from './gallery.js';
 
 const PHOTO_COUNT = 25;
-
-// Генерирует массив объектов с данными фотографий
 const generatedPhotos = createPhotos(PHOTO_COUNT);
 renderThumbnails(generatedPhotos);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const picturesContainer = document.querySelector('.pictures');
+
+  initGallery(picturesContainer, generatedPhotos);
+});
+
+closedBigPicture();
