91f10b2653a84cbea44278a7284356a2749ab490
Yarnara <edeven.gothery@gmail.com>
2024-12-23 18:00:32 +0700

    пишет функции для проверки условий

--- js/validate.js
+++ js/validate.js
@@ -6,3 +6,23 @@ const INVALID_HASHTAG_UNIQUE = 'Пожалуйста, убедитесь, что
 const INVALID_HASHTAG_SYMBOLS = 'Хэш-тег должен начинаться с # и содержать только буквы и цифры (до 20 символов).';
 const INVALID_HASHTAG_COUNT = 'Вы можете использовать не более 5 хэш-тегов.';
 const INVALID_CAPTION = 'Комментарий не должен превышать 140 символов.';
+
+// Приводит хэш-теги к единому формату
+function formatsHashtags(value) {
+  return value.trim().toLowerCase().split(' ').filter((hashtag) => hashtag);
+}
+
+// Проверяет правильность хэш-тегов
+function isRightHashtags(value) {
+  return formatsHashtags(value).every((hashtag) => HASHTAGS_REGEXP.test(hashtag));
+}
+
+// Проверяет количество хэш-тегов
+function isMaxCountHashtags(value) {
+  return createHashtags(value).length <= HASHTAGS_MAX_COUNT;
+}
+
+// Проверяет длину комментария
+function isValidCaption(value) {
+  return value.length < CAPTION_MAX_LENGTH;
+}
